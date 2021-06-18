3e675cebe06979b8d45d5e60ef49bbb8d1158fac
Maria M <prov21_1@hotmail.com>
2021-06-18 08:42:31 -0700

    mutations and queries.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,43 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+
 const resolvers = {
   Query: {
-    helloWorld: () => {
-      return "Hello world!";
+    //get a single user - me
+    me: async (_, __, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
+        return userData;
+      }
+      throw new AuthenticationError("You're not logged in.");
     },
   },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+
+      return user;
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      return user;
+    },
+  },
+  saveBook: async () => {},
+  deleteBook: async () => {},
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,7 +2,47 @@
 const { gql } = require("apollo-server-express");
 
 // create our typeDefs
-const typeDefs = gql``;
+const typeDefs = gql`
+  type Query {
+    me: User
+  }
+
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String!
+    authors: [Authors]
+    description: String
+    title: String!
+    image: Image
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    saveBook(
+      authors: [Authors]!
+      description: String!
+      bookId: Int!
+      image: Image!
+      link: Link!
+      title: String!
+    ): User
+    removeBook(bookId: Int!): User
+    addUser(username: String!, email: String!, password: String!): Auth
+  }
+`;
 
 // export the typeDefs
 module.exports = typeDefs;
