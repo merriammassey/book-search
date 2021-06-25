1bdee58929cd9bfda25097afeef540ae16a47f29
Maria M <prov21_1@hotmail.com>
2021-06-24 19:09:38 -0700

    production

--- server/server.js
+++ server/server.js
@@ -20,12 +20,12 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-/* // if we're in production, serve client/build as static assets
+//if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.use(routes); */
+//app.use(routes);
 
 db.once("open", () => {
   app.listen(PORT, () => {
