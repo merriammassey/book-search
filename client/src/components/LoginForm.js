37e910515031073f6a9e77d8c0718302b10107b4
Maria M <prov21_1@hotmail.com>
2021-06-24 20:39:25 -0700

    changed to apollo/client

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -5,7 +5,7 @@ import { Form, Button, Alert } from "react-bootstrap";
 //import { loginUser } from "../utils/API";
 import Auth from "../utils/auth";
 //add useMutation
-import { useMutation } from "@apollo/react-hooks";
+import { useMutation } from "@apollo/client";
 import { LOGIN_USER } from "../utils/mutations";
 import { loginUser } from "../utils/API";
 
--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -4,7 +4,7 @@ import { Form, Button, Alert } from "react-bootstrap";
 //import { createUser } from "../utils/API";
 import Auth from "../utils/auth";
 //add useMutation
-import { useMutation } from "@apollo/react-hooks";
+import { useMutation } from "@apollo/client";
 import { ADD_USER } from "../utils/mutations";
 
 const SignupForm = () => {
